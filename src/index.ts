4f170610478b124e02cfae8366aab3f280e86410
aansmirnov <aansmrnv@gmail.com>
2024-05-17 22:51:06 +0300

    feat: add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import './styles/index.css'
-import './styles/reset.css'
+import './styles/index.css';
+import './styles/reset.css';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <h1>Flappy Doge!</h1>
-`
+`;
