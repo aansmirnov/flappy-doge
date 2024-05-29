ba75e406a93aacf1c3e57cf8340874881d4c5a1a
aansmirnov <aansmrnv@gmail.com>
2024-05-29 22:30:09 +0300

    chore: update info block text

--- src/pages/game/index.ts
+++ src/pages/game/index.ts
@@ -7,7 +7,8 @@ export function gamePage() {
           <div class="block">
               <h3 class="block__title">Flappy Dode</h3>
               <p class="block__description">
-                Play flappy doge here online for free. Click on the screen, or use your spacebar to get started.
+                Play flappy doge here online for free. 
+                Use your spacebar to get started.
                 Fly the doge as far as you can without hitting a pipe.
               </p>
               <h3 class="block__record">Your record: <span id="game_score">0</span></h3>
