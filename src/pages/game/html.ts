b4924e1ea85334d639cba99b32a349b9fa1cd7de
aansmirnov <aansmrnv@gmail.com>
2024-08-06 01:56:16 +0300

    refactor: remove dumb styles for game page

--- src/pages/game/html.ts
+++ src/pages/game/html.ts
@@ -2,15 +2,15 @@ import { GAME_PAGE_ID } from '@/consts';
 
 export const GAME_PAGE = `
     <div id=${GAME_PAGE_ID} class="game">
-    <canvas id="canvas"></canvas>
-    <div class="block">
-        <h3 class="block__title">Flappy Doge</h3>
-        <p class="block__description">
-            Play flappy doge here online for free. 
-            Use your spacebar to get started.
-            Fly the doge as far as you can without hitting a pipe.
-        </p>
-        <h3 class="block__record">Your record: <span id="game-score"></span></h3>
-    </div>
+        <canvas id="canvas"></canvas>
+        <div class="block">
+            <h3 class="block__title">Flappy Doge</h3>
+            <p class="block__description">
+                Play flappy doge here online for free. 
+                Use your spacebar to get started.
+                Fly the doge as far as you can without hitting a pipe.
+            </p>
+            <h3 class="block__record">Your record: <span id="game-score"></span></h3>
+        </div>
     </div>
 `.trim();
